76dacd45b5936dfb82bd6ae7df668b5303fc2df4
root <vicous@free.fr>
2025-07-21 23:17:16 +0200

    change script

--- envrionnement/dev/scripts/install_argocd.sh
+++ envrionnement/dev/scripts/install_argocd.sh
@@ -2,20 +2,32 @@
 
 set -e
 
-# Add Argo Helm repo
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
+echo "1. Création du namespace argocd..."
+kubectl create namespace argocd || echo "Namespace argocd existe déjà"
 
-# Install Argo CD into 'argocd' namespace
-helm install argocd argo/argo-cd --namespace argocd --create-namespace
+echo "2. Installation d'Argo CD via les manifests officiels..."
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Get NodePort and IP
+echo "3. Attente que les pods soient prêts..."
+kubectl wait --for=condition=available --timeout=180s deployment/argocd-server -n argocd
+
+echo "4. Patch du service argocd-server en NodePort (pour accès local)..."
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "NodePort"}}'
+
+echo "5. Récupération du mot de passe admin initial..."
+PASSWORD=$(kubectl get secret -n argocd argocd-initial-admin-secret \
+  -o jsonpath="{.data.password}" | base64 -d)
+
+echo "Mot de passe admin : $PASSWORD"
+
+echo "6. Pour accéder à l'interface Web Argo CD :"
 NODE_PORT=$(kubectl get svc argocd-server -n argocd -o jsonpath='{.spec.ports[0].nodePort}')
 NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
 echo "URL : https://${NODE_IP}:${NODE_PORT}"
 echo "Utilisateur : admin"
-
-# Get initial password
-echo "Mot de passe initial :"
-kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
-
+echo "|||||||||||||||||||||||||||||"
+echo "Argo CD installé avec succès."
+echo "|||||||||||||||||||||||||||||"
+echo "Utiliser cette commande pour accéder à l'interface Web :"
+echo "kubectl port-forward svc/argocd-server -n argocd 8080:443"
+echo "localhost:8080"
