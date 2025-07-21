369803e7b3c8108b924cee8443476628cc3c16bd
Nicolas Tasserit <nicolas.tasserit@epitech.eu>
2025-07-21 21:06:32 +0200

    Create makefile for install Argocd in cluster Minikube

---
+++ envrionnement/dev/scripts/install_argocd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+# Add Argo Helm repo
+helm repo add argo https://argoproj.github.io/argo-helm
+helm repo update
+
+# Install Argo CD into 'argocd' namespace
+helm install argocd argo/argo-cd --namespace argocd --create-namespace
+
+# Get NodePort and IP
+NODE_PORT=$(kubectl get svc argocd-server -n argocd -o jsonpath='{.spec.ports[0].nodePort}')
+NODE_IP=$(kubectl get nodes -o jsonpath='{.items[0].status.addresses[?(@.type=="InternalIP")].address}')
+echo "URL : https://${NODE_IP}:${NODE_PORT}"
+echo "Utilisateur : admin"
+
+# Get initial password
+echo "Mot de passe initial :"
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
