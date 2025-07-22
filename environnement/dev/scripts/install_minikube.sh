d4704a043ef94b9d3acc2e296663c6a11e60fbd3
Romain Bernier <romain.bernier@live.fr>
2025-07-22 10:41:26 +0200

    changed resolv.conf

--- environnement/dev/scripts/install_minikube.sh
+++ environnement/dev/scripts/install_minikube.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e 
+set -e
 
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
@@ -9,6 +9,10 @@ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
 sudo rm -rf kubectl kubectl.sha256
 sudo curl -LO https://github.com/kubernetes/minikube/releases/latest/download/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube 
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
 sudo rm minikube-linux-amd64
+sudo tee /etc/resolv.conf <<EOF
+nameserver 8.8.8.8
+nameserver 1.1.1.1
+EOF
 minikube start
