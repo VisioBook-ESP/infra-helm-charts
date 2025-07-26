e27b35fad215d746fc1da2a7d9f217437160a9f9
root <vicous@free.fr>
2025-07-27 01:08:17 +0200

    tst

--- environnement/dev/scripts/deploy_applications.sh
+++ environnement/dev/scripts/deploy_applications.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 kubectl apply -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
+
+
+#Istio
+kubectl create namespace istio-system
+
 kubectl apply -f scripts/configs/argocd/istio-base-app.yaml -n argocd
 kubectl apply -f scripts/configs/argocd/istio-control-plane-app.yaml -n argocd
 kubectl apply -f scripts/configs/argocd/istio-gateway-app.yaml -n argocd
