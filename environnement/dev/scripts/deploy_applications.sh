82d778a3aca2079057a71ca916f7476b2238de48
root <vicous@free.fr>
2025-07-27 00:47:01 +0200

    tst

---
+++ environnement/dev/scripts/deploy_applications.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl apply -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
+kubectl apply -f scripts/configs/argocd/istio-base-app.yaml -n argocd
+kubectl apply -f scripts/configs/argocd/istio-control-plane-app.yaml -n argocd
+kubectl apply -f scripts/configs/argocd/istio-gateway-app.yaml -n argocd
