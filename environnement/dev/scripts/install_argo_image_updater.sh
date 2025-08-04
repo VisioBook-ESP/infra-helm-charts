d9cc48f2414c320ac3055e2b0552c5ee75a51a9e
root <vicous@free.fr>
2025-08-04 23:38:39 +0200

    fix

--- environnement/dev/scripts/install_argo_image_updater.sh
+++ environnement/dev/scripts/install_argo_image_updater.sh
@@ -7,5 +7,3 @@ kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argo
 
 # # Install Argo CD Image Updater
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
-
-kubectl apply -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
