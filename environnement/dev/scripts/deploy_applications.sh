d0b93c8acfd4564a7fc2d11a5f7f8340c97eef6b
root <vicous@free.fr>
2025-08-26 14:46:51 +0200

    test

--- environnement/dev/scripts/deploy_applications.sh
+++ environnement/dev/scripts/deploy_applications.sh
@@ -5,5 +5,8 @@ kubectl apply -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
 
 #Istio
 kubectl create namespace istio-system
+kubectl apply --server-side -f https://raw.githubusercontent.com/prometheus-operator/prometheus-operator/v0.85.0/bundle.yaml
 
 kubectl apply -f scripts/configs/argocd/istio-app.yaml -n argocd
+# kubectl apply -f scripts/configs/argocd/kiali-app.yaml -n argocd
+# kubectl apply -f scripts/configs/argocd/prometheus-app.yaml -n argocd
