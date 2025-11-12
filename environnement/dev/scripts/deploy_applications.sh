aef5bcb4a5c3c302cd7bd0e13c78c168a632b5c7
TasseritNicolas <nicolas.tasserit@epitech.eu>
2025-11-12 23:50:11 +0100

    feat(): create redis charts

--- environnement/dev/scripts/deploy_applications.sh
+++ environnement/dev/scripts/deploy_applications.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-#kubectl apply -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
+Istio
+kubectl create namespace istio-system
 
+pwd
+ls
+cd app/configs/istio
 
-#Istio
-#kubectl create namespace istio-system
-#
-#pwd
-#ls
-#cd app/configs/istio
-#
-#pwd
-#
-#kubectl apply -f istio-base.yaml -n istio-system
-#kubectl apply -f istiod.yaml --validate=false -n istio-system --wait
-#kubectl apply -f istio-ingress.yaml -n istio-system
-#
-#sleep 60
-#
-#kubectl apply -f istio-addons/prometheus.yaml -n istio-system
-#kubectl apply -f istio-addons/grafana.yaml -n istio-system
-#kubectl apply -f istio-addons/kiali.yaml -n istio-system
-#kubectl apply -f istio-addons/jaeger.yaml -n istio-system
+pwd
+
+kubectl apply -f istio-base.yaml -n istio-system
+kubectl apply -f istiod.yaml --validate=false -n istio-system --wait
+kubectl apply -f istio-ingress.yaml -n istio-system
+
+sleep 60
+
+kubectl apply -f istio-addons/prometheus.yaml -n istio-system
+kubectl apply -f istio-addons/grafana.yaml -n istio-system
+kubectl apply -f istio-addons/kiali.yaml -n istio-system
+kubectl apply -f istio-addons/jaeger.yaml -n istio-system
 #
 kubectl create namespace database
+kubectl label namespace database istio-injection=enabled
+
+
 
 #kubectl apply -f istio-gateway/ecommerce-gateway.yaml
--- environnement/dev/scripts/install_argo_image_updater.sh
+++ environnement/dev/scripts/install_argo_image_updater.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
-kubectl delete -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
-
-# Uninstall Image Updater
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
-
-# # Install Argo CD Image Updater
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
-#kubectl create secret docker-registry ghcr-creds \
-#  --namespace=argocd \
-#  --docker-server=ghcr.io \
-#  --docker-username="$GHCR_USER" \
-#  --docker-password="$GHCR_TOKEN" \
-#  --docker-email="$GHCR_EMAIL" || echo "Secret GHCR existe déjà"
+#
+##kubectl delete -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
+#
+## Uninstall Image Updater
+#kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
+#
+## # Install Argo CD Image Updater
+#kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
+##kubectl create secret docker-registry ghcr-creds \
+##  --namespace=argocd \
+##  --docker-server=ghcr.io \
+##  --docker-username="$GHCR_USER" \
+##  --docker-password="$GHCR_TOKEN" \
+##  --docker-email="$GHCR_EMAIL" || echo "Secret GHCR existe déjà"
+echo "coucou"
