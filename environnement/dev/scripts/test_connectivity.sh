59a1778b1843fc0eca0f0c3020a22c4f3e17a8f9
vicous6 <vicous@free.fr>
2025-11-28 14:33:29 +0100

    test

--- environnement/dev/scripts/test_connectivity.sh
+++ environnement/dev/scripts/test_connectivity.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-curl -v http://support-storage-service-support-storage-service:80/health
-curl -v http://core-user-service-core-user-service:80/health
+touch /tmp/test-connectivity
+cd /tmp/test-connectivity
+
+echo "Testing connectivity to services..." > /tmp/test-connectivity/
+
+curl -v http://support-storage-service-support-storage-service:80/health >> /tmp/test-connectivity/
+curl -v http://core-user-service-core-user-service:80/health >> /tmp/test-connectivity/
