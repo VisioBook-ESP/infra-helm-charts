4cf94ae3a6a4977989863eea8a986288a3d0d9e4
TasseritNicolas <nicolas.tasserit@epitech.eu>
2025-12-05 10:01:14 +0100

    test

--- environnement/dev/scripts/deploy_applications.sh
+++ environnement/dev/scripts/deploy_applications.sh
@@ -29,10 +29,10 @@ cd ..
 
 kubectl apply -f argocd/postgresql-app.yaml
 #kubectl apply -f argocd/postgresql-app-test.yaml
-kubectl apply -f argocd/mongodb-app.yaml
+#kubectl apply -f argocd/mongodb-app.yaml
 kubectl apply -f argocd/redis-app.yaml
 kubectl apply -f argocd/core-user-service.yaml
-#kubectl apply -f argocd/ai-analysis-service.yaml
+kubectl apply -f argocd/ai-analysis-service.yaml
 #kubectl apply -f argocd/support-storage-service.yaml
 
 
