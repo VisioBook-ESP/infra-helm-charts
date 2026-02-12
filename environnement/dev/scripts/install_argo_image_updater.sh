f44faa8ef4770b98a141af70f34ef022b365505c
vicous <vicous@free.fr>
2026-02-13 00:36:56 +0100

    test

--- environnement/dev/scripts/install_argo_image_updater.sh
+++ environnement/dev/scripts/install_argo_image_updater.sh
@@ -3,7 +3,7 @@
 ##kubectl delete -f scripts/configs/argocd/nginx-test-app.yaml -n argocd
 #
 ## Uninstall Image Updater
-kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
+kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/config/install.yaml
 #
 ## # Install Argo CD Image Updater
-kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/master/manifests/install.yaml
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj-labs/argocd-image-updater/stable/config/install.yaml
