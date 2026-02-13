2e0cc706ff54bc853bc4ac8023e038962753531b
vicous <vicous@free.fr>
2026-02-13 12:11:04 +0100

    test tls

--- environnement/dev/scripts/test-fonctionnel.sh
+++ environnement/dev/scripts/test-fonctionnel.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 IP=visiobook.cloud
-curl -X POST https://$IP:9999/api/v1/auth/register -H "Content-Type: application/json" -d '{"email":"user@example.com","password":"SecurePassword123!","username":"myusername"}'
-curl -X POST https://$IP:9999/api/v1/auth/login -H "Content-Type: application/json" -d '{"email":"user@example.com","password":"SecurePassword123!"}'
+curl -X POST https://$IP/api/v1/auth/register -H "Content-Type: application/json" -d '{"email":"user@example.com","password":"SecurePassword123!","username":"myusername"}'
+curl -X POST https://$IP/api/v1/auth/login -H "Content-Type: application/json" -d '{"email":"user@example.com","password":"SecurePassword123!"}'
