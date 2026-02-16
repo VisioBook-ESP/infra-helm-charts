7bdf2849371574892624d73fc701dcb86f2cc8ca
vicous6 <92452177+vicous6@users.noreply.github.com>
2026-02-16 23:57:21 +0100

    44 configurer la gateway pour core user service (#45)
    * test gateway
    
    * test gateway
    
    * configure gateway done
    
    * amélioration script de test role user

--- environnement/dev/scripts/test-fonctionnel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-IP=visiobook.cloud
-curl -X POST https://$IP/api/v1/auth/register -H "Content-Type: application/json" -d '{"email":"user@example.com","password":"SecurePassword123!","username":"myusername"}'
-curl -X POST https://$IP/api/v1/auth/login -H "Content-Type: application/json" -d '{"email":"user@example.com","password":"SecurePassword123!"}'
